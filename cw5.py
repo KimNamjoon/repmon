d5170b9b557ce8bf3f7e95849a24f12517ed6a8f
KimNamjoon <31881701+KimNamjoon@users.noreply.github.com>
2017-11-14 11:53:31 +0300

    Add files via upload
    classwork 14.11

---
+++ cw5.py
@@ -0,0 +1,11 @@
+with open('C:/Users/student/Desktop/bts.txt', encoding='utf-8') as f:
+    text = f.read()
+CONS ='бвгджзклмнпрстфхцчшщ'
+VOV = 'аеёиоуыэюя'
+v = 0
+##for i in word:
+##    letter = i.lower()
+##    if letter == "a" or letter == "e" or\
+##       letter == "i" or letter == "o" or\
+##       letter == "u" or letter == "y":
+##        v += 1
