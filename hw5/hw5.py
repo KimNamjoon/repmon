e570bf438ce7e4e2c3159dcf6328ec76968c15fd
KimNamjoon <akim_1@edu.hse.ru>
2017-12-06 22:57:39 +0300

    hw5

---
+++ hw5/hw5.py
@@ -0,0 +1,7 @@
+w = 'слово' #w - word
+while len(w) > 0:
+    w = input ('Введите слово: ')
+    if len(w) > 5:
+        with open("file.txt", "a", encoding="utf-8") as f:
+            f.write(w)
+            f.write("\n")
