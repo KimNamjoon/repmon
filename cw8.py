37980afe58d85c185287a5e7787e9d84ddbff490
KimNamjoon <akim_1@edu.hse.ru>
2018-01-25 18:01:16 +0300

    cw8

---
+++ cw8.py
@@ -0,0 +1,13 @@
+with open("text.txt", encoding="utf-8") as f:
+    text = f.read()
+    text.strip(',') 
+    text = text.split()
+    
+a = text
+d = {}
+for word in a:
+    if word in d:
+        d[word] += 1
+    else:
+        d[word] = 1
+print(d)
