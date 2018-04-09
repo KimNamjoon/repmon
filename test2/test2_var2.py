0a33cd22966b0b0e87f98b5e0a1de919d027ca75
KimNamjoon <akim_1@edu.hse.ru>
2018-04-09 17:07:59 +0300

    забыла написать вариант

--- test2/test2_var2.py
+++ test2/test2_var2.py
@@ -1,4 +1,7 @@
 import re
+import collections
+
+##variant_2
 
 def file_name():
     name = input('Введите название файла с расширением: ')
@@ -24,15 +27,38 @@ def task1():
 def preparing_text(name):
     with open (name, 'r', encoding='utf-8') as f:
         txt = f.read()
-return txt
+    return txt
 
-def find_nouns(txt):
-    first_match_n = re.search('<w lemma="[a-zA-z]" type="n[kvhx][ef][noþe][g]?[s]?"', txt)
-    n_group = first_match_n.group(1)
-    print(n_group)
+##def find_nouns(txt):
+##    first_match_n = re.search('<w lemma="[a-zA-z]" type="n[kvhx][ef][noþe][g]?[s]?"', txt)
+##    n_group = first_match_n.group(1)
+##    print(n_group)
 
+def find_them(txt):
+    they = re.findall('lemma=".*" type="(.*)"', txt)
+    return they
 
+def count_them_2(they):
+    count = {}
+    c = collections.Counter(they)
+    count.update(c)
+    return count
+
+##def write_in_dictionary(count):
+##    with open('dictionary.txt', 'w', encoding='utf-8') as f2
+
+##def task2():
+##    name = file_name()
+##    txt = preparing_text(name)
+##    they = find_them(txt)
+##    count = count_them_2(they)
+##    write_in_dictionary(count)
+##
+        
 task1()
+
+
+
 
     
     
